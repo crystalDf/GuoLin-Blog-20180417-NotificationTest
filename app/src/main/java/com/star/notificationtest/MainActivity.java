f8977ac964fc08946522a745ee8fa2553f9de9fe
crystalDf <chendong333@gmail.com>
2018-05-24 23:17:01 +0800

    Notification Test

--- app/src/main/java/com/star/notificationtest/MainActivity.java
+++ app/src/main/java/com/star/notificationtest/MainActivity.java
@@ -1,7 +1,13 @@
 package com.star.notificationtest;
 
+import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            String channelId = "chat";
+            String channelName = "聊天消息";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            createNotificationChannel(channelId, channelName, importance);
+
+            channelId = "subscribe";
+            channelName = "订阅消息";
+            importance = NotificationManager.IMPORTANCE_DEFAULT;
+            createNotificationChannel(channelId, channelName, importance);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createNotificationChannel(String channelId, String channelName, int importance) {
+
+        NotificationChannel notificationChannel =
+                new NotificationChannel(channelId, channelName, importance);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        Objects.requireNonNull(notificationManager).createNotificationChannel(notificationChannel);
     }
 }
