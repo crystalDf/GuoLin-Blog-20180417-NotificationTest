33dba406720fa152fdd0e9a6b5b7443b41496e72
crystalDf <chendong333@gmail.com>
2018-05-26 23:40:04 +0800

    Notification Test

--- app/src/main/java/com/star/notificationtest/MainActivity.java
+++ app/src/main/java/com/star/notificationtest/MainActivity.java
@@ -1,15 +1,22 @@
 package com.star.notificationtest;
 
 import android.annotation.TargetApi;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mChatButtom;
+    private Button mSubscribeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,33 @@ public class MainActivity extends AppCompatActivity {
             importance = NotificationManager.IMPORTANCE_DEFAULT;
             createNotificationChannel(channelId, channelName, importance);
         }
+
+        mChatButtom = findViewById(R.id.chat);
+        mSubscribeButton = findViewById(R.id.subscribe);
+        mChatButtom.setOnClickListener(v -> {
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            Notification notification = new NotificationCompat.Builder(this, "chat")
+                    .setContentTitle("收到一条聊天消息")
+                    .setContentText("今天中午吃什么？")
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground))
+                    .setAutoCancel(true)
+                    .build();
+            notificationManager.notify(1, notification);
+        });
+        mSubscribeButton.setOnClickListener(v -> {
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            Notification notification = new NotificationCompat.Builder(this, "subscribe")
+                    .setContentTitle("收到一条订阅消息")
+                    .setContentText("地铁沿线30万商铺抢购中！")
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground))
+                    .setAutoCancel(true)
+                    .build();
+            notificationManager.notify(2, notification);
+        });
     }
 
     @TargetApi(Build.VERSION_CODES.O)
