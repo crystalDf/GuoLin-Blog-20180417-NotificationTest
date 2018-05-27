d48db3d6a01876f358b5ad47df44784f8eefcbd0
crystalDf <chendong333@gmail.com>
2018-05-27 23:50:03 +0800

    Notification Test

--- app/src/main/java/com/star/notificationtest/MainActivity.java
+++ app/src/main/java/com/star/notificationtest/MainActivity.java
@@ -4,12 +4,15 @@ import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -39,6 +42,18 @@ public class MainActivity extends AppCompatActivity {
         mSubscribeButton = findViewById(R.id.subscribe);
         mChatButtom.setOnClickListener(v -> {
             NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel notificationChannel = notificationManager.getNotificationChannel("chat");
+                if (notificationChannel.getImportance() == NotificationManager.IMPORTANCE_NONE) {
+                    Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+                    intent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
+                    intent.putExtra(Settings.EXTRA_CHANNEL_ID, notificationChannel.getId());
+                    startActivity(intent);
+                    Toast.makeText(this, "请手动将通知打开", Toast.LENGTH_SHORT).show();
+                }
+            }
+
             Notification notification = new NotificationCompat.Builder(this, "chat")
                     .setContentTitle("收到一条聊天消息")
                     .setContentText("今天中午吃什么？")
